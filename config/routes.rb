ce2b72e2599ae000139e171a3474bea681352f30
chance <chancesmith97@hotmail.com>
2020-12-05 13:09:37 -0700

    namespaced controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :events
-  resources :days
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    namespace :v1 do 
+      resouces :days 
+      resouces :events 
+    end
+  end
 end
