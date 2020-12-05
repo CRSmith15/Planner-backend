2ff2ed371b0404c7ab3705bdcb7c53c8bbe99504
chance <chancesmith97@hotmail.com>
2020-12-05 12:50:03 -0700

    ran migrations

--- db/migrate/20201205194416_create_events.rb
+++ db/migrate/20201205194416_create_events.rb
@@ -1,7 +1,6 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
-      t.integer :day_id
       t.string :taks
       t.string :description
       t.belongs_to :day, null: false, foreign_key: true
